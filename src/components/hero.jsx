d27e1bb47d8551512ba0c3df069b3d91c278a5fa
Eggward12 <cristian.sanchez.casd@gmail.com>
2024-03-06 17:32:26 -0500

    si

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,13 +2,13 @@ export const Hero = () => {
   return (
     <>
       <main className="bg-[#171717] h-screen w-screen">
-        <div className="flex justify-between">
+        <div className="flex justify-between p-4">
           <button className="bg-white font-bold px-2 py-1 rounded-2xl">
             Descubre premium
           </button>
           <button className="bg-black text-white font-bold px-2 py-1 rounded-2xl">Instalar app</button>
         </div>
-        <img className="rounded-lg" src="https://mp3teca.co/-/images/26004_1400_20230825004731-300x300.jpg" alt="" />
+        <img className="rounded-lg p-2 w-[10%]" src="https://mp3teca.co/-/images/26004_1400_20230825004731-300x300.jpg" alt="" />
       </main>
     </>
   );
