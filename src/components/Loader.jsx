c36bcc5285263d60444171c38ff5086efa1dc259
Eggward12 <cristian.sanchez.casd@gmail.com>
2024-05-11 23:33:34 -0500

    loader mobile

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="w-32 h-32 animate-spin rounded-full border-l-green-500 border-4 mt-5 fixed left-[45%]"></div>
+    <div className="w-32 h-32 animate-spin rounded-full border-l-green-500 border-4 mt-5 fixed md:left-[45%] left-[40%]"></div>
   );
 };
 
