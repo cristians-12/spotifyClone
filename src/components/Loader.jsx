1bec1a9e737c5392c97bf548c4edb8a391ee4c62
Eggward12 <cristian.sanchez.casd@gmail.com>
2024-05-11 23:25:14 -0500

    loaderr

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="flex animate-spin justify-center pt-10 relative">
-      <div className="w-32 h-32 rounded-full border-l-green-500 border-4 mt-5"></div>
-    </div>
+    <div className="w-32 h-32 animate-spin rounded-full border-l-green-500 border-4 mt-5 fixed left-[45%]"></div>
   );
 };
 
