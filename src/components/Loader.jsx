a64a28221e338dc8c2e0d94d96f493339a9aef96
Eggward12 <cristian.sanchez.casd@gmail.com>
2024-05-11 22:37:15 -0500

    loader arreglado

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loader = () => {
   return (
-    <div>Cargando</div>
-  )
-}
+    <div className="flex animate-spin justify-center">
+      <div className="w-32 h-32 rounded-full border-l-green-500 border-4"></div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
